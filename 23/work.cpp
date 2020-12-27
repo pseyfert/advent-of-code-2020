ec23bbc65fac595d50c79fcdba4e5f85b777f49c
Paul Seyfert <pseyfert.mathphys@gmail.com>
2020-12-27 11:18:51 +0100

    potential optimizations

--- 23/work.cpp
+++ 23/work.cpp
@@ -1,26 +1,33 @@
 // #include <boost/circular_buffer.hpp>
 // #include <boost/circular_buffer/base.hpp>
+#include <algorithm>
 #include <array>
+#include <boost/progress.hpp>
 #include <compare>
 #include <iostream>
 #include <string>
 #include <vector>
 
+long int CUPVALUES_END = 10;
+
 struct cupvalue {
-  int val;
-  explicit cupvalue(int v) {
-    if (v > 0) {
-      val = v % 10;
+  long int val;
+  explicit operator long() { return val; }
+  explicit cupvalue(long int v) {
+    if (v == 0) {
+      val = CUPVALUES_END - 1;
+    } else if (v > 0) {
+      val = v % CUPVALUES_END;
     } else {
-      int add_at_least = -v;
-      while ((add_at_least % 10) != 0) {
+      long int add_at_least = -v;
+      while ((add_at_least % CUPVALUES_END) != 0) {
         add_at_least++;
       }
-      if (add_at_least > 10) {
+      if (add_at_least > CUPVALUES_END) {
         std::cout << "huh?\n";
       }
       val = v + add_at_least;
-      if (val < 0 || val >= 10) {
+      if (val < 0 || val >= CUPVALUES_END) {
         std::cout << "BIG mistake 2\n";
       }
     }
@@ -30,12 +37,12 @@ struct cupvalue {
   cupvalue &operator=(cupvalue &&) = default;
   cupvalue(const cupvalue &) = default;
   cupvalue(cupvalue &&) = default;
-  friend cupvalue operator-(const cupvalue &lhs, int rhs) {
+  friend cupvalue operator-(const cupvalue &lhs, long int rhs) {
     return cupvalue(lhs.val - rhs);
   }
   cupvalue &operator--() {
     if (val == 0) {
-      val = 9;
+      val = CUPVALUES_END - 1;
     } else {
       val--;
     }
@@ -55,6 +62,7 @@ struct cupvalue {
 };
 
 auto parseinput(int i) {
+  CUPVALUES_END = 10;
   std::vector<cupvalue> reverse;
   std::vector<cupvalue> b;
   while (i > 0) {
@@ -64,6 +72,14 @@ auto parseinput(int i) {
   for (auto it = reverse.rbegin(); it != reverse.rend(); it++) {
     b.push_back(*it);
   }
+  // part 2
+  long int next_insert = 10;
+  b.reserve(1000000);
+  CUPVALUES_END = 1000100; // a bit generous
+  while (b.size() < 1000000) {
+    b.push_back(cupvalue(next_insert++));
+  }
+  CUPVALUES_END = next_insert;
   return b;
 }
 
@@ -76,19 +92,31 @@ template <typename T> void printcontainer(const T &t) {
   std::cout << '}' << std::endl;
 }
 
+std::size_t percent(std::size_t a, std::size_t b) {
+  if (a < b)
+    return a;
+  a -= b;
+  if (a < b)
+    return a;
+  return a % b;
+}
+
 template <typename T> void step(T &buffer, std::size_t current) {
+  current = percent(current, buffer.size());
   // fix underflow
-  auto seekval = buffer[(current) % 9] - 1;
+  auto seekval = buffer[percent(current, buffer.size())] - 1;
   // TODO: put more descriptive thing for current + buffer.size()
   std::size_t target = current + buffer.size();
   auto seeker = [current, buffer](const cupvalue &seekval) {
     // std::cout << "seeking " << seekval << '\n';
-    for (std::size_t scan = current + 4; scan < current + buffer.size();
-         ++scan) {
+    // for (std::size_t scan = current + 4; scan < current + buffer.size();
+    //      ++scan) {
+    for (std::size_t scan = current + buffer.size() - 1; scan >= current + 4;
+         --scan) {
       // std::cout << "checking if at position " << scan << " there is a " <<
-      // seekval << '\n'; std::cout << "i see a " << buffer[(scan)%9] <<
-      // "there\n";
-      if (seekval == buffer[(scan) % 9]) {
+      // seekval << '\n'; std::cout << "i see a " <<
+      // buffer[(scan)%buffer.size()] << "there\n";
+      if (seekval == buffer[percent(scan, buffer.size())]) {
         // target = scan;
         return scan;
       }
@@ -96,9 +124,31 @@ template <typename T> void step(T &buffer, std::size_t current) {
     // std::cout << "nope\n";
     return current + buffer.size();
   };
-  int seek_n_times = 0;
-  while (target == current + buffer.size()) {
+
+  std::array<cupvalue, 3> moving;
+  for (std::size_t i = 0; i < 3; ++i) {
+    moving[i] = buffer[percent(current + i + 1, buffer.size())];
+  }
+
+  int seek_n_times = 1;
+  while (std::find(moving.begin(), moving.end(), seekval) != moving.end()) {
     seek_n_times++;
+    --seekval;
+  }
+  if (seek_n_times > 3) {
+    std::cout << "this shouldn't have happened" << std::endl;
+    std::cout << "started searching at "
+              << buffer[percent(current, buffer.size())] << "-1\n";
+    std::cout << "decremented " << seek_n_times << "times\n";
+    std::cout << "pick up values are:";
+    // printcontainer(moving);
+    std::cout << "setup is:";
+    // printcontainer(buffer);
+  }
+
+  int actual_seeks = 0;
+  while (target == current + buffer.size()) {
+    actual_seeks++;
     // std::cout << "searching for " << seekval << '\n';
     // std::cout << "current pointer is " << current << '\n';
     target = seeker(seekval);
@@ -109,26 +159,38 @@ template <typename T> void step(T &buffer, std::size_t current) {
     // NB: do not use after loop!
     --seekval;
     // std::cout << " to " << seekval << '\n';
-  }
-  std::array<cupvalue, 3> moving;
-  for (std::size_t i = 0; i < 3; ++i) {
-    moving[i] = buffer[(current + i + 1) % 9];
-  }
-  if (seek_n_times > 3) {
-    std::cout << "this shouldn't have happened" << std::endl;
-    std::cout << "started searching at " << buffer[(current) % 9] << "-1\n";
-    std::cout << "decremented " << seek_n_times << "times\n";
-    std::cout << "pick up values are:";
-    printcontainer(moving);
-    std::cout << "setup is:";
-    printcontainer(buffer);
+    if (actual_seeks > 1) {
+      std::cout << "this shouldn't have happened, either" << std::endl;
+      std::cout << "started searching at "
+                << buffer[percent(current, buffer.size())] << "-1\n";
+      std::cout << "searched " << actual_seeks << " times\n";
+      std::cout << "pick up values are:";
+      // printcontainer(moving);
+      std::cout << "setup is:";
+      // printcontainer(buffer);
+    }
   }
 
-  for (std::size_t m = current + 4; m <= target; ++m) {
-    buffer[(m - 3) % 9] = buffer[(m) % 9];
+  {
+    if (target > 2 * buffer.size()) {
+      std::cout << "optimization assumption violated\n";
+    }
+    std::size_t m;
+    // source and destination don't need modulo
+    for (m = current + 4; m <= target && m < buffer.size(); ++m) {
+      buffer[m - 3] = buffer[m];
+    }
+    // source needs modulo, destination doesn't
+    for (; m <= target && (m - 3) < buffer.size(); ++m) {
+      buffer[percent(m - 3, buffer.size())] = buffer[percent(m, buffer.size())];
+    }
+    // source and destination need one modulo
+    for (; m <= target; ++m) {
+      buffer[(m - 3) - buffer.size()] = buffer[(m)-buffer.size()];
+    }
   }
   for (std::size_t i = 0; i < 3; ++i) {
-    buffer[(target - 2 + i) % 9] = moving[i];
+    buffer[percent(target - 2 + i, buffer.size())] = moving[i];
   }
 }
 
@@ -140,11 +202,28 @@ int main(int argc, char **argv) {
   }
   int input = std::stol(argv[1]);
   auto game = parseinput(input);
-  printcontainer(game);
-  for (std::size_t steps = 0; steps < 100; steps++) {
+  boost::progress_display prog(10000000);
+  // printcontainer(game);
+  for (std::size_t steps = 0; steps < 10000000; steps++) {
     step(game, steps);
+    ++prog;
     // printcontainer(game);
   }
-  printcontainer(game);
+  // printcontainer(game);
+  auto one = std::find(game.begin(), game.end(), cupvalue{1});
+  if (one == game.end()) {
+    std::cout << "messed up\n";
+  }
+  if (one + 1 == game.end()) {
+    std::cout << "a little difficult\n";
+  } else {
+    std::cout << "number after 1 is " << *(one + 1) << '\n';
+  }
+  if (one + 2 == game.end()) {
+    std::cout << "a little harder\n";
+  } else {
+    std::cout << "number two after 1 is " << *(one + 2) << '\n';
+  }
+  std::cout << "answer is " << long(*(one + 1)) * long(*(one + 2)) << '\n';
   return 42;
 }
