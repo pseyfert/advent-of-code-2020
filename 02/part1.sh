4a114826a17fb14e0c51284b5ba74d8e1ebcd471
Paul Seyfert <pseyfert.mathphys@gmail.com>
2020-12-27 22:18:28 +0100

    part 1 of day 2

--- 02/part1.sh
+++ 02/part1.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/zsh
-line="1-3 a: abcde"
-count=0
-for i in {1..${#line##*: }} ; do
-  current_letter=${${line##*: }[$i]}
-  echo $current_letter
-  seek=${${line//: *}#* }
-  echo $seek
-  [[ $current_letter = $seek ]] && echo "match" || echo "nooope"
-done
+
+good_passwords=0
+while read line; do
+  count=0
+  min=${line//-*}
+  max=${${line#*-}// *}
+  for i in {1..${#line##*: }} ; do
+    current_letter=${${line##*: }[$i]}
+    seek=${${line//: *}#* }
+    [[ $current_letter = $seek ]] && ((count++))
+  done
+  # echo "counted $count times, which should be between $min and $max"
+  (($count<=$max && $count>=$min)) && ((good_passwords++))
+done < input.txt
+
+echo "$good_passwords were valid"
